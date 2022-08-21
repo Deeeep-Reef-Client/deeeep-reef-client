606ff4cdc775bd5c9c3fb254320eeb2b1bc61a88
Tuna <101097344+Canned-Seagull23@users.noreply.github.com>
2022-08-21 19:00:32 +0800

    Rich presence now shows gamemode
    cleaned some useless comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const path = require('path');
 const RPC = require('discord-rpc');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-// eslint-disable-next-line global-require
 if (require('electron-squirrel-startup')) {
     app.quit();
 }
@@ -17,11 +15,7 @@ const createWindow = () => {
             preload: path.join(__dirname, 'preload.js'),
         },
     });
-
-    // and load the index.html of the app.
     window.loadURL("https://deeeep.io");
-
-    // Open the DevTools.
     window.webContents.openDevTools();
     window.maximize();
     window.removeMenu();
@@ -39,14 +33,14 @@ const createWindow = () => {
     Menu.setApplicationMenu(menu);
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+        createWindow();
+        ipcMain.on("gamemode", (event: Event, gamemode: string) => {
+            setDiscordActivity(gamemode);
+        })
+    }
+);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
@@ -54,24 +48,24 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
 
-let rpc = new RPC.Client({
-    transport: 'ipc'
-});
-rpc.login({ clientId: "1006552150807150594" });
-rpc.on('ready', () => {
+function setDiscordActivity(gamemode: string) {
     rpc.setActivity({
         details: "Playing Deeeep.io",
         largeImageKey: "favicon-big",
         largeImageText: "Deeeep.io",
-        // smallImageKey: "ffa",
-        // smallImageText: "Playing Unknown Gamemode",
+        smallImageKey: gamemode.toLowerCase(),
+        smallImageText: gamemode,
         startTimestamp: new Date(),
-      })
+    })
+};
+
+let rpc = new RPC.Client({
+    transport: 'ipc'
 });
+rpc.login({ clientId: "1006552150807150594" });
+rpc.on('ready', () => setDiscordActivity("Menu"));
--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,13 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { ipcRenderer } = require('electron');
+
+window.addEventListener("load", () => {
+    const btn = document.querySelector(".play");
+    const modes = document.querySelector('.block, .modes');
+    btn.addEventListener("click", () => {
+        ipcRenderer.send("gamemode", 
+            document.querySelector('.block, .modes')
+                .querySelector('.selected')
+                .querySelector('.name').innerText
+        );
+    })
+})
