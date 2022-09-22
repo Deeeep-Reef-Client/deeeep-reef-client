a95ae1f242e65dd034173a50cabf326e7c527584
Canned Tuna <101097344+Canned-Seagull23@users.noreply.github.com>
2022-09-22 22:16:54 +0800

    Add unfinished evolution tree

--- src/preload.js
+++ src/preload.js
@@ -135,35 +135,38 @@ window.addEventListener("DOMContentLoaded", () => {
         characterData: false,
         subtree: true
     });
-    // Evolution tree
-    /*
-    const sidePaneTop = document.querySelector("div.p-2.sidebar.right.space-y-2 > .container > div.el-row.justify-center") as HTMLDivElement;
-    const treeButtonContainer = sidePaneTop!.querySelector("div")!.cloneNode(true) as HTMLDivElement;
-    const treeButton = treeButtonContainer.firstElementChild as HTMLButtonElement;
-    const treeName = treeButton!.querySelector("span > span.inner") as HTMLSpanElement;
-    const treeIcon = treeButton!.querySelector("span > svg") as HTMLElement;
-    treeButton!.classList.remove("pink");
-    treeButton!.classList.add("black");
-    treeName!.innerText = "Tree";
+    // Evolution tree button
+    const sidePaneTop = document.querySelector("div.p-2.sidebar.right.space-y-2 > .container > div.el-row.justify-center");
+    const treeButtonContainer = sidePaneTop.querySelector("div").cloneNode(true);
+    const treeButton = treeButtonContainer.firstElementChild;
+    const treeName = treeButton.querySelector("span > span.inner");
+    const treeIcon = treeButton.querySelector("span > svg");
+    treeButton.classList.remove("pink");
+    treeButton.classList.add("black");
+    treeName.innerText = "Tree";
     treeIcon.outerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-diagram-3-fill" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zm-6 8A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm6 0A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1z"/>
   </svg>`;
     treeIcon.remove();
     sidePaneTop.appendChild(treeButtonContainer);
-
     // Evolution tree modal
-    const treeStyle = document.createElement("style");
-    document.head.appendChild(treeStyle);
-    treeStyle.innerHTML = `.tree-content { border-radius: .75rem;--tw-shadow-color: 0,0,0;--tw-shadow: 0 25px 50px -12pxrgba(var(--tw-shadow-color), 0.25);-webkit-box-shadow: var(--tw-ring-offset-shadow,0 0 #0000),var(--tw-ring-shadow,0 0 #0000),var(--tw-shadow);box-shadow: var(--tw-ring-offset-shadow,0 0 #0000),var(--tw-ring-shadow,0 0 #0000),var(--tw-shadow);padding-top: .5rem;padding-bottom: .5rem;padding-left: .75rem;padding-right: .75rem;--tw-bg-opacity: 1;background-color: rgba(255,255,255,var(--tw-bg-opacity));--tw-border-opacity: 1;border-color: rgba(243,244,246,var(--tw-border-opacity));position: relative;display: flex;flex-direction: column;max-height: 90%;margin: 0 1rem;border: 1px solid;min-width: 20rem; }`;
     const treeDiv = document.createElement("div");
-    document.getElementById("app")!.appendChild(treeDiv);
-    treeDiv.outerHTML = `<div id="treeModal" style="z-index:1000" class="w-screen h-screen"></div>`;
-    const treeModal = document.getElementById("treeModal") as HTMLDivElement;
-    const treeContentDiv = document.createElement("div");
-    treeModal.appendChild(treeContentDiv);
-    treeContentDiv.outerHTML = `<div id="treeContent" class="modal-content"><img src="https://deeeep-reef-client.netlify.app/assets/evolution_tree.png" alt="Deeeep.io v4 beta evolution tree"></img></div>`;
-    const treeContent = document.getElementById("treeContent") as HTMLDivElement;
-*/
+    document.getElementById("app").appendChild(treeDiv);
+    treeDiv.outerHTML = `<div style="z-index: 10000;" class="vfm__content modal-content">
+    <span class="modal__title">
+        <div class="justify-self-center">Evolution Tree</div>
+    </span>
+    <div class="modal__content">
+        <img src="https://deeeep-reef-client.netlify.app/assets/evolution_tree.png"
+            alt="Deeeep.io v4 beta evolution tree"></img>
+        <button class="modal__close"><svg width="1.125em" height="1.125em" viewBox="0 0 24 24" class="svg-icon"
+                color="gray" style="--sx:1; --sy:1; --r:0deg;">
+                <path
+                    d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z">
+                </path>
+            </svg></button>
+    </div>
+</div>`;
     // Watch for match start
     const btn = document.querySelector(".play");
     btn.addEventListener("click", () => {
--- src/preload.ts
+++ src/preload.ts
@@ -142,8 +142,7 @@ window.addEventListener("DOMContentLoaded", () => {
         subtree: true
     });
 
-    // Evolution tree
-    /*
+    // Evolution tree button
     const sidePaneTop = document.querySelector("div.p-2.sidebar.right.space-y-2 > .container > div.el-row.justify-center") as HTMLDivElement;
     const treeButtonContainer = sidePaneTop!.querySelector("div")!.cloneNode(true) as HTMLDivElement;
     const treeButton = treeButtonContainer.firstElementChild as HTMLButtonElement;
@@ -155,22 +154,43 @@ window.addEventListener("DOMContentLoaded", () => {
     treeIcon.outerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-diagram-3-fill" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zm-6 8A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm6 0A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1z"/>
   </svg>`;
-    treeIcon.remove();
     sidePaneTop.appendChild(treeButtonContainer);
 
     // Evolution tree modal
     const treeStyle = document.createElement("style");
+    treeStyle.innerHTML = `.tree-title{margin-bottom:.5rem;margin-right:1rem;text-align:center;display:flex;align-items:center;justify-content:space-between;font-size:1.3rem;line-height:1.2em;font-weight:300;z-index:1}.dark .tree-title{color:rgba(229,231,235,1)}.tree-content{flex-grow:1;overflow-y:auto;height:100%;display:flex;justify-content:center}.tree-close{position:absolute;top:.3rem;right:.5rem}`;
     document.head.appendChild(treeStyle);
-    treeStyle.innerHTML = `.tree-content { border-radius: .75rem;--tw-shadow-color: 0,0,0;--tw-shadow: 0 25px 50px -12pxrgba(var(--tw-shadow-color), 0.25);-webkit-box-shadow: var(--tw-ring-offset-shadow,0 0 #0000),var(--tw-ring-shadow,0 0 #0000),var(--tw-shadow);box-shadow: var(--tw-ring-offset-shadow,0 0 #0000),var(--tw-ring-shadow,0 0 #0000),var(--tw-shadow);padding-top: .5rem;padding-bottom: .5rem;padding-left: .75rem;padding-right: .75rem;--tw-bg-opacity: 1;background-color: rgba(255,255,255,var(--tw-bg-opacity));--tw-border-opacity: 1;border-color: rgba(243,244,246,var(--tw-border-opacity));position: relative;display: flex;flex-direction: column;max-height: 90%;margin: 0 1rem;border: 1px solid;min-width: 20rem; }`;
     const treeDiv = document.createElement("div");
     document.getElementById("app")!.appendChild(treeDiv);
-    treeDiv.outerHTML = `<div id="treeModal" style="z-index:1000" class="w-screen h-screen"></div>`;
-    const treeModal = document.getElementById("treeModal") as HTMLDivElement;
-    const treeContentDiv = document.createElement("div");
-    treeModal.appendChild(treeContentDiv);
-    treeContentDiv.outerHTML = `<div id="treeContent" class="modal-content"><img src="https://deeeep-reef-client.netlify.app/assets/evolution_tree.png" alt="Deeeep.io v4 beta evolution tree"></img></div>`;
-    const treeContent = document.getElementById("treeContent") as HTMLDivElement;
-*/
+    treeDiv.outerHTML = `<div id="treeModal" style="z-index:10000" class="vfm__content modal-content">
+    <span class="tree-title">
+        <div></div>
+        <div class="justify-self-center">Evolution Tree</div>
+        <div></div>
+    </span>
+    <div class="tree-content">
+        <img src="https://deeeep-reef-client.netlify.app/assets/evolution_tree.png"
+            alt="Deeeep.io v4 beta evolution tree">
+    </div>
+    <button id="treeCloseButton" class="tree-close"><svg width="1.125em" height="1.125em" viewBox="0 0 24 24"
+            class="svg-icon" color="gray"
+            style="--sx:1; --sy:1; --r:0deg;">
+            <path
+                d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+            ></path>
+        </svg></button>
+</div>
+`;
+    const treeModal = document.getElementById("treeModal");
+    const treeCloseButton = document.getElementById("treeCloseButton");
+    // Tree button onclick
+    treeButton.addEventListener("click", () => {
+        treeModal!.classList.toggle("tree-hidden");
+    });
+    treeCloseButton!.addEventListener("click", () => {
+        treeModal!.classList.toggle("tree-hidden");
+    });
+
     // Watch for match start
     const btn = document.querySelector(".play");
     btn!.addEventListener("click", () => {
@@ -212,7 +232,7 @@ window.addEventListener("DOMContentLoaded", () => {
                     characterData: false,
                     subtree: true
                 });
-                
+
             }
         });
         openObserver.observe(element!, {
