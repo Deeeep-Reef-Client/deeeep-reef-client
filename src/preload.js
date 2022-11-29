fffb6c874a72d899e6a069a133e34c5bb63c629a
Canned Tuna <101097344+Canned-Seagull23@users.noreply.github.com>
2022-11-29 21:30:14 +0800

    make swap account button

--- src/preload.js
+++ src/preload.js
@@ -397,6 +397,31 @@ window.addEventListener("DOMContentLoaded", () => {
         characterData: false,
         subtree: true
     });
+    // Account swapper
+    const userWidget = document.querySelector("div.user-widget");
+    const loginButton = userWidget.querySelector("button.el-button.btn.nice-button.blue.has-icon");
+    loginButton.addEventListener("click", () => {
+        const loginObserver = new MutationObserver((mutations) => {
+            if (!document.contains(document.querySelector("div.modal__action > div#routeModalActions > button.el-button.btn.nice-button.gray")))
+                return;
+            loginObserver.disconnect();
+            const routeModalActions = document.getElementById("routeModalActions");
+            // switch account button
+            const swapAccountButton = document.querySelector("div.modal__action > div#routeModalActions > button.el-button.btn.nice-button.gray").cloneNode(true);
+            swapAccountButton.querySelector("span[class] > span.inner").innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+</svg>
+            `;
+            routeModalActions.insertBefore(swapAccountButton, routeModalActions.childNodes[2]);
+        });
+        loginObserver.observe(document.getElementById("app"), {
+            attributes: false,
+            childList: true,
+            characterData: false,
+            subtree: true
+        });
+    });
     // Save name
     document.querySelector("div.name-input > div.el-input__wrapper > input.el-input__inner").addEventListener("change", (e) => {
         settings.gameName = e.target.value;
