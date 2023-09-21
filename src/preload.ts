92405ffbd679a3041c43880c394dd9cf7d4ffc29
Canned Tuna <101097344+Canned-Seagull23@users.noreply.github.com>
2023-09-21 20:27:18 +0800

    Update evolution tree hotkey display on keybinds change

--- src/preload.ts
+++ src/preload.ts
@@ -967,6 +967,21 @@ window.addEventListener("DOMContentLoaded", () => {
         keybindsDisplayGhostQuit!.innerText = settings.keybinds.ghostQuit;
         keybindsDisplayCopyUrl!.innerText = settings.keybinds.copyUrl;
         keybindsDisplayJoinGame!.innerText = settings.keybinds.joinGame;
+
+        const treeHotkeyElem = document.getElementById("treeOpenHotkey");
+        if (gameStarted && document.contains(treeHotkeyElem)) {
+            let gameTreeHotkey;
+
+            if (settings.keybinds.evolutionTree.startsWith("Key")) {
+                gameTreeHotkey = settings.keybinds.evolutionTree.slice(3);
+            } else if (settings.keybinds.evolutionTree.startsWith("Digit")) {
+                gameTreeHotkey = settings.keybinds.evolutionTree.slice(5);
+            } else {
+                gameTreeHotkey = settings.keybinds.evolutionTree.slice(0, 1);
+            }
+            
+            treeHotkeyElem!.innerText = gameTreeHotkey;
+        }
     }
 
     function changeKeybind(button: HTMLButtonElement, keybind: (keyof typeof settings.keybinds)) {
@@ -8018,7 +8033,7 @@ THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WIT
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-diagram-3-fill" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zm-6 8A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm6 0A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1z"/>
   </svg>
-                <div class="drc-hotkey hotkey drc-hotkey--dark hotkey--dark hotkey">${gameTreeHotkey}</div>
+                <div id="treeOpenHotkey" class="drc-hotkey hotkey drc-hotkey--dark hotkey--dark hotkey">${gameTreeHotkey}</div>
                 `;
 
                     gameTreeButton.addEventListener("click", () => {
