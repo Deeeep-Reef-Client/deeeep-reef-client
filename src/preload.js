56cc480f57ef1d087e88cf3865f0231884ad5628
Canned Tuna <101097344+Canned-Seagull23@users.noreply.github.com>
2023-10-30 17:41:37 +0800

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -7753,7 +7753,7 @@ THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WIT
                 });
                 chatInputObserver.observe(chatInputElem, { attributes: true });
                 function ghostSuicide(key) {
-                    if (key.code != settings.keybinds.ghostQuit || homePageOpened || !document.contains(document.querySelector("div.chat-input.horizontal-center[style='display: none;']")))
+                    if (key.code != settings.keybinds.ghostQuit || homePageOpened || chatInputOpened)
                         return;
                     DRC.Preload.evalInBrowserContext(`
                     if (game.currentScene.myAnimal._visibleFishLevel == 33) {
@@ -7763,7 +7763,7 @@ THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WIT
                 }
                 ;
                 function cancelBoost(key) {
-                    if (key.code != settings.keybinds.cancelCharge || homePageOpened || !document.contains(document.querySelector("div.chat-input.horizontal-center[style='display: none;']")))
+                    if (key.code != settings.keybinds.cancelCharge || homePageOpened || chatInputOpened)
                         return;
                     DRC.Preload.evalInBrowserContext(`
                     game.inputManager.pressElapsed = 0;
@@ -7772,14 +7772,14 @@ THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WIT
                 }
                 ;
                 function boostKeybindKeydown(key) {
-                    if (settings.keybinds.boost === "Space" || homePageOpened || key.code !== settings.keybinds.boost || !document.contains(document.querySelector("div.chat-input.horizontal-center[style='display: none;']")))
+                    if (settings.keybinds.boost === "Space" || homePageOpened || key.code !== settings.keybinds.boost || chatInputOpened)
                         return;
                     DRC.Preload.evalInBrowserContext(`
                     game.inputManager.pointerDown = true;
                     `);
                 }
                 function boostKeybindKeyup(key) {
-                    if (settings.keybinds.boost === "Space" || homePageOpened || key.code !== settings.keybinds.boost || !document.contains(document.querySelector("div.chat-input.horizontal-center[style='display: none;']")))
+                    if (settings.keybinds.boost === "Space" || homePageOpened || key.code !== settings.keybinds.boost || chatInputOpened)
                         return;
                     DRC.Preload.evalInBrowserContext(`
                     document.querySelector("div.game > div#canvas-container > canvas").dispatchEvent(new MouseEvent("pointerup", { clientX: ${canvasMouseX}, clientY: ${canvasMouseY} }));
@@ -7789,8 +7789,7 @@ THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WIT
                     if (key.code !== settings.keybinds.screenshot
                         || !document.contains(document.querySelector("#canvas-container > canvas"))
                         || homePageOpened
-                        || !document.contains(document.querySelector("div.chat-input.horizontal-center[style='display: none;']"))
-                        || !document.contains(document.querySelector("div.home-page[style='display: none;']")))
+                        || chatInputOpened)
                         return;
                     ipcRenderer.send("captureScreenshot");
                     const overlayDiv = document.createElement("div");
